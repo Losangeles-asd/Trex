aa9c607f0b2e7b74b8cc537476cc7a3ba6e03130
Losangeles-asd <101433671+Losangeles-asd@users.noreply.github.com>
2022-06-28 20:31:07 -0400

    Add files via upload

---
+++ Trex_Stage_0-main/sketch.js
@@ -0,0 +1,34 @@
+
+var trex ,trex_running;
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png",);
+
+}
+
+function setup(){
+  createCanvas(600,200);
+
+  //create a trex sprite
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running",trex_running);
+
+  //create ground sprite
+  ground = createSprite(200,180,400,20);
+   
+  //adding scale and position
+  trex.scale = 0.5;
+  trex.x = 50;
+}
+
+function draw(){
+  background("white");
+  if(keyDown("space"))
+  {
+    trex.velocityY = -10;
+  }
+
+  trex.velocityY = trex.velocityY + 0.5
+  trex.collide(ground)
+  drawSprites();
+
+}
